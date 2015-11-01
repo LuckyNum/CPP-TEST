f3398ebc2a9d5bd5446e3e07eda0205bdc5f6677
LuckyNum <571996804@qq.com>
2015-11-01 22:50:55 +0800

    C++类

---
+++ cpplesson1/cpplesson1/people.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "people.h"
+
+
+void people::mysay()
+{
+	printf("LuckyNum\n");
+	getchar();	//使程序进入等待
+}
---
+++ cpplesson1/cpplesson1/people.h
@@ -0,0 +1,7 @@
+#pragma once
+class people
+{
+public:
+	void mysay();
+};
+
